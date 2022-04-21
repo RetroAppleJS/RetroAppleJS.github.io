76b8f2ac4337f39e5c05e29827fb5c854a493b98
Hitchhiker <freddy.vandriessche@gmail.com>
2022-04-21 17:03:56 +0200

    Update EMU_apple2keys.js

--- res/EMU_apple2keys.js
+++ res/EMU_apple2keys.js
@@ -102,14 +102,14 @@ function apple2OnKeyHover(event)
             _o.EMU_keyb_timer = false;
             setTimeout(apple2OnKeyHover_out, 2000);
         break;
-        /*
+        
         case "touchstart":
+            alert(event.type);
             break;
-            if(t.style.opacity!=1) break;
-        */
+        
         case "click":
             //if(o.EMU_keyb_timer == false) break;
-            alert(_o.EMU_keyb_timer+" "+t.style.opacity)
+            //alert(_o.EMU_keyb_timer+" "+t.style.opacity)
 
             if(typeof(keymap[yc][xc])=="number")
             {
