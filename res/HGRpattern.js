ed8956e2f9f07a1718c55e18a8e5da1c49cc6b2a
Hitchhiker <freddy.vandriessche@gmail.com>
2022-07-23 12:21:47 +0200

    remove extra argument in this.calculate

--- res/HGRpattern.js
+++ res/HGRpattern.js
@@ -60,28 +60,28 @@ function PATTERN(idx,x,y)
       return Math.round(100*this.colorDistance(a,[avg,avg,avg])/avg)
     }
     this.set_prep = function(bits) { this.prep = Number(bits); this.pmax = Math.pow(2,2*this.prep)*3 }
-    this.calculate = function(p,x,y)
+    this.calculate = function(p,x,y) // p = pattern number
     {
-        var n  = this.prep;         // horizontal pattern repetition
-        var pm = (1<<n)-1;          // when all n bits should be lit        
-        var ba = [0,1,3];           // cycle through bit 7 
-        var b7 =  ba[(p>>(2*n))&3]  // bit 7 status is changing at every combination of every even and uneven rows e.g. 2 * 4 bits = 8 bits
+        var n  = this.prep;          // horizontal pattern repetition
+        var pm = (1<<n)-1;           // when all n bits should be lit        
+        var ba = [0,1,3];            // cycle through bit 7 
+        var b7 =  ba[(p>>(2*n))&3]   // bit 7 status is changing at every combination of every even and uneven rows e.g. 2 * 4 bits = 8 bits
 
         if((y&1)==0)
-          return [0,(p&pm)&(1<<(x%n))?1:0      ,(b7&1)*128];    // even rows
+          return [(p&pm)&(1<<(x%n))?1:0      ,(b7&1)*128];    // even rows
         else 
-          return [0,((p>>n)&pm)&(1<<(x%n))?1:0 ,(b7&2)*64];     // odd rows
+          return [((p>>n)&pm)&(1<<(x%n))?1:0 ,(b7&2)*64];     // odd rows
     }
 
     this.is_2x2 = function(patternID)
     {
       for(var y=0;y<2;y++)
         for(var x=0;x<2;x++)
-          if(this.calculate(patternID,x,y)[1] != this.calculate(patternID,x+2,y)[1]) return false;
+          if(this.calculate(patternID,x,y)[0] != this.calculate(patternID,x+2,y)[0]) return false;
 
       for(var x=0;x<2;x++)
         for(var y=0;y<2;y++)
-          if(this.calculate(patternID,x,y)[1] != this.calculate(patternID,x,y+2)[1]) return false;
+          if(this.calculate(patternID,x,y)[0] != this.calculate(patternID,x,y+2)[0]) return false;
 
       return true;
     }
@@ -92,64 +92,64 @@ function PATTERN(idx,x,y)
     {
       switch(idx)
       {
-        case 0:  return  [0,editable_map(x,y)[0],editable_map(x,y)[1]];
-        //case 1:  return  [0,eval(custom_fmla[0]),eval(custom_fmla[1])];
-        //case 2:  return  [0,false,0];               // black 1 
-        case 1:  return  [0,(x+1)%2==0,0];            // green 
-        case 2:  return  [0,x%2==0,0];                // purple
-        case 3:  return  [0,true,0];                  // white 1  
-        case 4:  return  [0,false,128];               // black 2
-        case 5:  return  [0,(x+1)%2==0,128];         // orange
-        case 6:  return  [0,x%2==0,128];              // blue
-        case 7:  return  [0,true,128];                // white2 2
+        case 0:  return  [editable_map(x,y)[0],editable_map(x,y)[1]];
+        //case 1:  return  [eval(custom_fmla[0]),eval(custom_fmla[1])];
+        //case 2:  return  [false,0];               // black 1 
+        case 1:  return  [(x+1)%2==0,0];            // green 
+        case 2:  return  [x%2==0,0];                // purple
+        case 3:  return  [true,0];                  // white 1  
+        case 4:  return  [false,128];               // black 2
+        case 5:  return  [(x+1)%2==0,128];         // orange
+        case 6:  return  [x%2==0,128];              // blue
+        case 7:  return  [true,128];                // white2 2
 
-        case 10: return  [0 ,(x+1)%2==0 && y%2==0,0];  // dark green 
-        case 11: return  [0 ,x%2==0 && y%2==0,0];      // dark purple       
-        case 12: return  [0 ,(x+1)%2==0 && y%2==0,128];// dark orange
-        case 13: return  [0 ,x%2==0 && y%2==0,128];    // dark blue
-        case 14: return  [0 ,(x+1)%2==0 || y%2==0,0];  // dark green 
-        case 15: return  [0 ,x%2==0 || y%2==0,0];      // dark purple    
-        case 16: return  [0 ,(x+1)%2==0 || y%2==0,128];// dark orange
-        case 17: return  [0 ,x%2==0 || y%2==0,128];    // dark blue
-        case 18: return  [0 ,y%2==0,0];
-        case 19: return  [0 ,(!(x&1)==0 && y%3==0) || (!(x&1)==0 && y%3==1) || (1 && y%3==2),y%3!=1?128:0 ];
+        case 10: return  [(x+1)%2==0 && y%2==0,0];  // dark green 
+        case 11: return  [x%2==0 && y%2==0,0];      // dark purple       
+        case 12: return  [(x+1)%2==0 && y%2==0,128];// dark orange
+        case 13: return  [x%2==0 && y%2==0,128];    // dark blue
+        case 14: return  [(x+1)%2==0 || y%2==0,0];  // dark green 
+        case 15: return  [x%2==0 || y%2==0,0];      // dark purple    
+        case 16: return  [(x+1)%2==0 || y%2==0,128];// dark orange
+        case 17: return  [x%2==0 || y%2==0,128];    // dark blue
+        case 18: return  [y%2==0,0];
+        case 19: return  [(!(x&1)==0 && y%3==0) || (!(x&1)==0 && y%3==1) || (1 && y%3==2),y%3!=1?128:0 ];
 
-        case 20: return  [0 ,(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,0];
-        case 21: return  [0 ,(x+1)%2==0,y%2!=0?128:0];
-        case 22: return  [0 ,(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,y%2!=0?128:0];
-        case 23: return  [0 ,x%2==0 && y%2==0 || (x+1)%2==0 && y%2!=0,y%2!=0?128:0];
-        case 24: return  [9 ,x%2==0,y%2!=0?128:0];
-        case 25: return  [0 ,(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,128];
-        case 26: return  [0 ,(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,0];
-        case 27: return  [0 ,(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,0];
-        case 28: return  [0 ,(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,128];
-        case 29: return  [0 ,(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,128];          
+        case 20: return  [(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,0];
+        case 21: return  [(x+1)%2==0,y%2!=0?128:0];
+        case 22: return  [(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,y%2!=0?128:0];
+        case 23: return  [x%2==0 && y%2==0 || (x+1)%2==0 && y%2!=0,y%2!=0?128:0];
+        case 24: return  [x%2==0,y%2!=0?128:0];
+        case 25: return  [(x+1)%2==0 && y%2==0 || x%2==0 && y%2!=0,128];
+        case 26: return  [(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,0];
+        case 27: return  [(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,0];
+        case 28: return  [(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,128];
+        case 29: return  [(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,128];          
 
-        case 30: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,0]; 
-        case 31: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && x%2!=0,y%2!=0?128:0];   
-        case 32: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,y%2!=0?128:0]; 
-        case 33: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x)%2==0)   || y%2!=0 && x%2!=0,y%2!=0?128:0];  
-        case 34: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x)%2==0)   || y%2!=0 && (x+1)%2!=0,y%2!=0?128:0]; 
-        case 35: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,128];  
-        case 36: return  [0 ,y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && x%2!=0,y%2==0?128:0];
-        case 37: return  [0 ,(x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+2)%4==0) && y%2!=0,y%2==0?128:0 ];
-        case 38: return  [0 ,(x>>1)%2==0 && y%2==0 || ((x+2)>>1)%2==0 && y%2!=0 ,0 ];
+        case 30: return  [y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,0]; 
+        case 31: return  [y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && x%2!=0,y%2!=0?128:0];   
+        case 32: return  [y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,y%2!=0?128:0]; 
+        case 33: return  [y%2==0 && ( (x>>1)%2!=0 || (x)%2==0)   || y%2!=0 && x%2!=0,y%2!=0?128:0];  
+        case 34: return  [y%2==0 && ( (x>>1)%2!=0 || (x)%2==0)   || y%2!=0 && (x+1)%2!=0,y%2!=0?128:0]; 
+        case 35: return  [y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && (x+1)%2!=0,128];  
+        case 36: return  [y%2==0 && ( (x>>1)%2!=0 || (x+1)%2==0) || y%2!=0 && x%2!=0,y%2==0?128:0];
+        case 37: return  [(x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+2)%4==0) && y%2!=0,y%2==0?128:0 ];
+        case 38: return  [(x>>1)%2==0 && y%2==0 || ((x+2)>>1)%2==0 && y%2!=0 ,0 ];
 
-        case 40: return  [0, (x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+3)%4==0) && y%2!=0,0 ];
-        case 41: return  [0, ((x+1)%2==0 || (x+2)%4==0) && y%2==0 || ((x+1)%2==0 || (x+4)%4==0) && y%2!=0,0 ];
-        case 42: return  [0, ((x+1)%2==0 || (x+2)%4==0) && y%2==0 || ((x+1)%2==0 || (x+4)%4==0) && y%2!=0,y%2==0?128:0 ];
-        case 43: return  [0, (x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+3)%4==0) && y%2!=0,y%2==0?128:0 ];
-        case 44: return  [0 ,y%2==0 && (x>>1)%2!=0 || y%2!=0,0];
+        case 40: return  [(x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+3)%4==0) && y%2!=0,0 ];
+        case 41: return  [((x+1)%2==0 || (x+2)%4==0) && y%2==0 || ((x+1)%2==0 || (x+4)%4==0) && y%2!=0,0 ];
+        case 42: return  [((x+1)%2==0 || (x+2)%4==0) && y%2==0 || ((x+1)%2==0 || (x+4)%4==0) && y%2!=0,y%2==0?128:0 ];
+        case 43: return  [(x%2==0 || (x+1)%4==0) && y%2==0 || (x%2==0 || (x+3)%4==0) && y%2!=0,y%2==0?128:0 ];
+        case 44: return  [y%2==0 && (x>>1)%2!=0 || y%2!=0,0];
 
-        case 50: return  [0 ,(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,(y&1)<<7];
-        case 51: return  [0 ,(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,(y&1)<<7];
-        case 52: return  [0 ,(y&1)==0 && ((x+1)&3)==0,0];
-        case 53: return  [0 ,(y&1)==0 && (x&3)==0,0];
-        case 54: return  [0 ,(y&1)==0 && ((x+1)&3)==0,128];
-        case 55: return  [0 ,(y&1)==0 && (x&3)==0,128];
+        case 50: return  [(y&1)==0 && (x&3)==1 || (y&1)==1 && (x&3)==3 ,(y&1)<<7];
+        case 51: return  [(y&1)==0 && (x&3)==0 || (y&1)==1 && (x&3)==2 ,(y&1)<<7];
+        case 52: return  [(y&1)==0 && ((x+1)&3)==0,0];
+        case 53: return  [(y&1)==0 && (x&3)==0,0];
+        case 54: return  [(y&1)==0 && ((x+1)&3)==0,128];
+        case 55: return  [(y&1)==0 && (x&3)==0,128];
 
-        case "": return  [0,true,0];
-        default: return  [null,false,0];  
+        case "": return  [true,0];
+        default: return  [false,0];  
       }
   }
   */
@@ -169,10 +169,10 @@ function PATTERN(idx,x,y)
       {
         for(var x=1;x<this.bmapx*m-1;x++)
         {
-          var left = this.calculate(patternID,x-1,y)[1];
-          var me = this.calculate(patternID,x,y)[1];
-          var right = this.calculate(patternID,x+1,y)[1];
-          var b7 = this.calculate(patternID,x,y)[2];
+          var left = this.calculate(patternID,x-1,y)[0];
+          var me = this.calculate(patternID,x,y)[0];
+          var right = this.calculate(patternID,x+1,y)[0];
+          var b7 = this.calculate(patternID,x,y)[1];
           var col = colorFN(x, y, left, me, right, b7);
           col_sum = [ col_sum[0] + parseInt(col.substring(1,3),16)
                     , col_sum[1] + parseInt(col.substring(3,5),16)
@@ -251,15 +251,15 @@ function PATTERN(idx,x,y)
   this.pixel = function(x,y,patternID,rbyt)
   {
       var wbyt = rbyt | (1 << x%7);                     // OR current bit with pixel bit
-      if(rbyt ==0 && rbyt&8 != this.calculate(patternID,x,y)[2])
+      if(rbyt ==0 && rbyt&8 != this.calculate(patternID,x,y)[1])
         wbyt |= this.calculate(patternID,x,y)[2];    // OR high bit
       else
       {
-        wbyt |= this.calculate(patternID,x,y)[2];
+        wbyt |= this.calculate(patternID,x,y)[1];
         //document.getElementById("debug").innerHTML += "CONFLICT "+PRINTByte(rbyt)+" >> "+PRINTByte(wbyt)
       }  // TODO resolve Hi-Byte conflict !!
 
-      if(this.calculate(patternID,x,y)[1]) return wbyt;
+      if(this.calculate(patternID,x,y)[0]) return wbyt;
       return rbyt;
   }
 
@@ -287,8 +287,8 @@ function PATTERN(idx,x,y)
         var cc = typeof(this.criteria[p])=="object"?this.criteria[p]:{};
 
         var m=["0b",0,"0b",0]
-        for(var i=0;i<this.prep;i++) { var t=this.calculate(p,i,0); m[0]+=t[1];m[1]=t[2] }
-        for(var i=0;i<this.prep;i++) { var t=this.calculate(p,i,1); m[2]+=t[1];m[3]=t[2] }
+        for(var i=0;i<this.prep;i++) { var t=this.calculate(p,i,0); m[0]+=t[0];m[1]=t[1] }
+        for(var i=0;i<this.prep;i++) { var t=this.calculate(p,i,1); m[2]+=t[0];m[3]=t[1] }
         //console.log(p+" "+m[0]+" "+m[0]);
 
         var data_arg = {
