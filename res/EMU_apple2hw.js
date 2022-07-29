ad21b6722e05e8aba9b103fa31896d8914505de9
Hitchhiker <freddy.vandriessche@gmail.com>
2022-07-29 18:13:53 +0200

    Update EMU_apple2hw.js

--- res/EMU_apple2hw.js
+++ res/EMU_apple2hw.js
@@ -81,6 +81,9 @@ function Apple2Hw(vid) {
         // Randomize memory.  Don't do on reset!
         for (var i = 0; i < RAM_SIZE; i++)
             ram[i] = Math.floor(Math.random() * 256.0);
+        
+        // build memory map
+        this.build_mem_map();
     }
 
     this.cycle = function() {
@@ -92,49 +95,80 @@ function Apple2Hw(vid) {
     // methods.
     //
 
-    this.addr_decode = function(addr)
+    this.mem_layout = {
+        "0000-00FF":["#FFFFFF","ZERO-PAGE","ZP"]
+       ,"0100-01FF":["#E0E0E0","STACK","ST"]
+       ,"0200-02FF":["#00D000","GETLN buffer","BU"]
+       ,"0300-03FF":["#00D000","VECTORS","VC"]
+       ,"0400-07FF":["#DF48FF","TXT1/LORES1","T1"]
+       ,"0800-0BFF":["#D040E0","TXT2/LORES2","T2"]
+       ,"0C00-1FFF":["#00D000","APPLESOFT PRG","AP"]
+       ,"2000-3FFF":["#0000FF","HIRES1","H1"]
+       ,"4000-5FFF":["#0000E0","HIRES2","H2"]
+       ,"6000-BFFF":["rgba(0,0,0,0.1)","FREE","F"]
+       ,"C000-C0FF":["#B0B000","SLOT I/O","IO"]
+       ,"C100-C1FF":["#D0D000","SLOT 1 ROM","S1"]
+       ,"C200-C2FF":["#D0D000","SLOT 2 ROM","S2"]
+       ,"C300-C3FF":["#D0D000","SLOT 3 ROM","S3"]
+       ,"C400-C4FF":["#D0D000","SLOT 4 ROM","S4"]
+       ,"C500-C5FF":["#D0D000","SLOT 5 ROM","S5"]
+       ,"C600-C6FF":["#D0D000","SLOT 6 ROM","S6"]
+       ,"C700-C7FF":["#D0D000","SLOT 7 ROM","S7"]
+       ,"C800-CFFF":["#F0F000","SLOT ROM ext","SR"]         
+       ,"D000-F7FF":["#B00000","APPLESOFT ROM","AR"]
+       ,"F800-FFFF":["#D00000","MONITOR ROM","MR"]
+       //,"0300":["","GETLN","GL"]
+    }
+
+    this.mem_map = new Array(256);
+
+    this.build_mem_map = function()
     {
-        switch(addr >> 11 << 11)
+        for(var i in this.mem_layout)
         {
+            var a = i.split("-"); var b = [parseInt(a[0],16),parseInt(a[1],16)];
+            for(var addr=b[0];addr<b[1];addr+=parseInt("0100",16))
+            { 
+                this.mem_map[addr>>8] = this.mem_layout[i][2];
 
-            case 0xF000:      //Applesoft ROM 
-            case 0xE800:
-            case 0xE000:
-            case 0xD800:
-            case 0xD000:
-
-            break
-            case 0xC800:    // port ROM/RAM
-            break;
-            case 0xC000:    // I/O ports
-            break;
+                console.log("("+("0000"+addr.toString(16)).slice(-4).toUpperCase()+") "
+                +(addr>>8)+" "+this.mem_layout[i][2]);
+            }
         }
     }
 
     this.read = function(addr) {
         var d8;
+
         if(this.io.ramcard && this.io.ramcard.active == true && addr >= ROM_ADDR)
             d8 = this.io.read(addr);
         else if (addr < RAM_SIZE)
             d8 = ram[addr];
         else if (addr >= ROM_ADDR && addr < ROM_ADDR + ROM_SIZE)
-            d8 = apple2Rom[addr - ROM_ADDR];            
+            d8 = apple2Rom[addr - ROM_ADDR]; 
         else if (addr >= IO_ADDR && addr < IO_ADDR + IO_SIZE)
             d8 = this.io.read(addr - IO_ADDR);
         else
-            d8 = 0x55;
+                d8 = 0x55;  
+                /* 
+        else
+        {    
+            
 
-        // console.log("Apple2Hw.read(): %s %s",
-        //              addr.toString(16), d8.toString(16));
+            switch(this.mem_map[addr>>8])
+            {
+                case "IO": d8 = this.io.read(addr - IO_ADDR); break;
+                default:
+                    d8 = 0x55;
+            }
+        }*/
 
         return d8;
     }
 
     this.write = function(addr, d8) {
-        // console.log("Apple2Hw.write(): %s %s",
-        //            addr.toString(16), d8.toString(16));
-        if (d8 < 0 || d8 > 0xff) // XXX: Debug
-            console.err("Pet2001hw.write(%s %s) d8 too big!",
+        if (d8 < 0 || d8 > 0xff)
+            console.err("apple2hw.write(%s %s) d8 too big!",
                         addr.toString(16), d8.toString(16));
 
         if(this.io.ramcard &&  this.io.ramcard.active == true && addr >= ROM_ADDR)
