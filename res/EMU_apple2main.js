c689305b0200e36b832da95a8a860df59e799986
Hitchhiker <freddy.vandriessche@gmail.com>
2022-07-30 21:09:01 +0200

    Restart button next to disk loader

--- res/EMU_apple2main.js
+++ res/EMU_apple2main.js
@@ -2,31 +2,15 @@
 // Copyright (c) 2014 Thomas Skibo.
 // All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
+// Adapted in 2022 by Freddy Vandriessche.
+// notice: https://raw.githubusercontent.com/RetroAppleJS/RetroAppleJS.github.io/main/LICENSE.md
 //
-// THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE.
-//
+// apple2main.js 
 
-// apple2main.js
 
+// TODO FVD select here which system to run (Apple2plus - Apple2e - Apple2c ??) 
 // TODO FVD transform this into OOP
+
 
 addLoadEvent(EMU_init); // EMULATOR KICKSTART
 
@@ -53,7 +37,7 @@ console.log("CPU clock : "+_o.CPU_ClockTicks+" ticks in "+_o.EMU_IntervalTime_ms
 
 
 var ts = 0;
-var ppleIntervalHandle,vidContext,apple2plus,bKeyboardFocus;
+var appleIntervalHandle,vidContext,apple2plus,bKeyboardFocus;
 var bOsc = false;
 
 
