8a5c819bc201be0bcf6ddcc26d6e70ac6f5f6e8b
Hitchhiker <freddy.vandriessche@gmail.com>
2023-03-19 10:56:54 +0100

    Update data.js

--- tools/Tensorflow/data.js
+++ tools/Tensorflow/data.js
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import * as tf from './@tensorflow/tfjs';
+//import * as tf from './@tensorflow/tfjs';
 
 const IMAGE_SIZE = 784;
 const NUM_CLASSES = 10;
