70017fc7056789d8d338a4eeb2a93fa68a198ef3
Hitchhiker <freddy.vandriessche@gmail.com>
2024-02-06 20:17:31 +0100

    beep

--- tools/player-worklet.js
+++ tools/player-worklet.js
@@ -1,31 +1,42 @@
 class PlayerWorklet extends AudioWorkletProcessor {
-    constructor() {
+    constructor()
+    {
         super();
         this.port.onmessage = this.onmessage.bind(this);
         this.playing = false;
     }
 
-    onmessage(e) {
-        if (e.data.type === 'play') {
-            // Toggle between playing and silence
-            this.playing = !this.playing;
+    onmessage(e)
+    {
+        if (e.data.type === 'play')
+        {
+
+            this.playing = !this.playing;    // Toggle between playing and silence
             this.phase = 0;
         }
     }
 
-    process(inputs, outputs) {
+    nextOutput()
+    {
+        if (!this.playing) return 0.0;
+
+        this.phase += 0.1;
+        var out = Math.sin(this.phase);
+
+        return out;
+    }
+
+    process(inputs, outputs)
+    {
         const output = outputs[0];
         const channel = output[0];
 
-        for (let i = 0; i < channel.length; ++i) {
-            if (this.playing) {
-                channel[i] = Math.sin(this.phase);
-                this.phase += 0.1;
-            } else {
-                channel[i] = 0;
-            }
-        }
+        //this.port.postMessage({ message: 'l='+channel.length });
 
+        for (let i = 0; i < channel.length; ++i) {
+            const value = this.nextOutput();
+            channel[i] = value;
+        }
         return true;
     }
 }
