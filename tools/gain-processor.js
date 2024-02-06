8200c85f7dab0b85c16fbc30af7d8b0e660fa3ba
Hitchhiker <freddy.vandriessche@gmail.com>
2024-02-06 06:31:25 +0100

    AudioWorkletJS

---
+++ tools/gain-processor.js
@@ -0,0 +1,19 @@
+/* "processor.js" file. */
+class PortProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.port.onmessage = (event) => {
+      // Handling data from the node.
+      console.log(event.data);
+    };
+
+    this.port.postMessage('Hi!');
+  }
+
+  process(inputs, outputs, parameters) {
+    // Do nothing, producing silent output.
+    return true;
+  }
+}
+
+registerProcessor('port-processor', PortProcessor);
