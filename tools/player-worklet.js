1f267a682f111b966d192531122f679a0811d52b
Hitchhiker <freddy.vandriessche@gmail.com>
2024-02-08 14:09:07 +0100

    AudioWorklet

--- tools/player-worklet.js
+++ tools/player-worklet.js
@@ -5,38 +5,17 @@ class PlayerWorklet extends AudioWorkletProcessor
         super();
         this.port.onmessage = this.onmessage.bind(this);
         this.sampleData = new Array();
-        this.sampleBuffer = [];
-
-        this.BufferSel = -1;
-        this.BufferIdx = 0;
-        this.BufferLen = 2;
-        this.sampleIndex = 0;
+        this.sampleIndex    = 0;
     }
 
     onmessage(e)
     {
         switch(e.data.type)
         {
-            case "data":
-                this.sampleData = e.data.audio;
-                this.sampleIndex = e.data.audio.length - 1;
-                //this.port.postMessage({ message: 'data='+e.data.audio.length });
-            break;
-            case "play":
-                this.sampleData = e.data.audio;
-                this.sampleIndex = e.data.audio.length - 1;
-            break;
             case "append":
-                this.sampleData =  e.data.audio.concat(this.sampleData);
+                this.sampleData  =  e.data.audio.concat(this.sampleData);
                 this.sampleIndex += e.data.audio.length - 1;
-            break;
-            case "double_buffer":
-                this.BufferSel++;
-                if(this.BufferSel==this.BufferLen) this.BufferSel = 0;
-
-                this.sampleBuffer[this.BufferSel] =  e.data.audio.concat(this.sampleData);
-                //this.sampleIndex += e.data.audio.length - 1;
-            break;            
+            break;         
         }
     }
 
@@ -46,11 +25,7 @@ class PlayerWorklet extends AudioWorkletProcessor
         if(this.sampleIndex > 0)
         { 
             this.sampleIndex--;    // sampleIndex = 0 means buffer empty
-            if(this.sampleIndex==0)
-            {
-                this.port.postMessage({ message: 'buffer empty' });
-                this.sampleData = new Array();
-            }
+            if(this.sampleIndex==0) { this.sampleData = new Array(); this.port.postMessage({ message: 'empty' }); }
             return sample;
         }
         else return 0.0
@@ -59,9 +34,8 @@ class PlayerWorklet extends AudioWorkletProcessor
     process(inputs, outputs)
     {
         const output = outputs[0];
-        const channel = output[0];
+        const channel = output[0];    //this.port.postMessage({ message: 'l='+channel.length });
 
-        //this.port.postMessage({ message: 'l='+channel.length });
         for (let i = 0; i < channel.length; ++i)
         {
             const value = this.nextOutput();
