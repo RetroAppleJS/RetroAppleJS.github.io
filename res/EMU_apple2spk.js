5426c6f512c9065be8c569554c8f8348649e2de7
Hitchhiker <freddy.vandriessche@gmail.com>
2024-02-10 17:03:36 +0100

    Update EMU_apple2spk.js

--- res/EMU_apple2spk.js
+++ res/EMU_apple2spk.js
@@ -12,30 +12,33 @@ function AppleSpeaker()
 {
     // every bit in this array represents speaker status 0 or 1 for one emulator cycle
     //this.buffer = new Uint8Array(_o.CPU_ClocksTicks_s / _o.EMU_Updates_s / 8);
+    this.bDebug     = true;
     this.state      = 0;             
     this.AWblockL   = 128;        // Length of 1 AudioWorklet data block.  Currently, audio data blocks are always 128 frames long = 128 32-bit floating-point samples for each channel
     this.AWblockN   = 34;         // Number of AudioWorklet data blocks in buffer
-    this.samplerate = 43520;    // Speaker Emulation Sample rate (must be an integer factor of block length)
+    this.samplerate = 43520;      // Speaker Emulation Sample rate (must be an integer factor of block length)
     this.data = new Array(this.AWblockL * this.AWblockN);
 
     this.tickCycle = Math.round(_o.CPU_ClocksTicks_s / this.samplerate); // Ticks per emulator cycle
     // 20*128 samp / 25600 samp/sec  = 0.1 sec = length of one EMULATOR cycle
 
     //alert(Math.round(this.tickCycle))
-
     this.audio = null;
     this.player = null;
     this.data = new Array(this.AWblockL * this.AWblockN - 4).fill(0);
     this.cnt = 0;
+    this.data_i = {};
+    var ccnt = 0;
+    var floatval = 0;
 
     this.init = async function(bEnable)
     {
         if(bEnable)
         {
             this.audio = new AudioContext({ latencyHint: 'interactive', sampleRate: this.samplerate });
-            await this.audio.audioWorklet.addModule('res/AudioWorklet.js');  // Load an audio worklet
+            await this.audio.audioWorklet.addModule('res/AudioWorklet.js');       // Load an audio worklet
             this.player = new AudioWorkletNode(this.audio,'emulator-worklet');    // Create a player
-            this.player.connect(this.audio.destination);                        // Connect the player to the audio context     
+            this.player.connect(this.audio.destination);                          // Connect the player to the audio context     
         }
         else
         {
@@ -48,26 +51,37 @@ function AppleSpeaker()
     {
         this.n = n;
         var m = n % this.tickCycle;
+        
         if(m==0)
         {
-            this.cnt++;
             this.pos--;
+            this.pval = this.val;
+            this.val = this.state-0.5;
 
-            this.data[this.pos] = this.state-0.5;
+           
+            if(this.val==this.pval)
+            { 
+                ccnt++;
+                if(ccnt > 100)
+                {
+                    floatval *= 0.99
+                }
+            }
+            else
+            {
+                floatval = this.val;
+                ccnt = 0;
+            }
+            this.data[this.pos] = floatval;
 
+            if(this.cnt==1)
+                this.data_i[ this.val ] = this.data_i[ this.val ]===undefined?1:(this.data_i[ this.val ]+1);
         }
-        //this.data[]
+    }
 
-        // 4348
+    this.filter = function()
+    {
 
-        /*
-        // store sound data in ringbuffer
-        var byte_idx = n >> 3;
-        var bit_idx = n % 8;
-        var bit_msk = this.state << bit_idx;
-        var val = bit_idx==7 ? bit_msk : this.buffer[byte_idx] | bit_msk;
-        this.buffer[byte_idx] = val;
-        */
     }
 
     this.toggle = function()
@@ -81,25 +95,32 @@ function AppleSpeaker()
 
         // 4348 (-4)  - calculated =  4352
         //console.log("cnt"+this.cnt);
-        this.cnt=0;
+
+        if(this.bDebug && this.cnt==1)
+        {
+            //console.log("pos="+this.pos); //document.getElementById("debug").innerHTML += "pos="+this.pos+"<br>"
+            var s = "";
+            for(var i in this.data_i)
+            {
+                s += i+" x"+this.data_i[i]+"  ";
+            }
+            console.log(s);
+            this.data_i = {}
+        }
+
+        this.cnt = (this.cnt%10) + 1;
         this.pos = this.data.length;
 
         this.player.port.postMessage({ type:'append', audio: this.data });
 
-        /*
-        var audio_data = calc_wave( _o.EMU_AWblockN * _o.EMU_AWblockL, 4 );
-        player.port.postMessage({ type:'append', audio: audio_data });    // append data to audio buffer
-        if(audio.state!="running") { oLap.chrono(true); audio.resume() };
-
-        player.port.onmessage = (e) => 
+        this.player.port.onmessage = (e) => 
         {
             if(e.data.message=="empty")
             {
-            var lap = oLap.chrono(false);
-            if(bDebug) document.getElementById("dump").innerHTML += "<br>"+lap+"ms";
-            audio.suspend();
+                //var lap = oLap.chrono(false);
+                //if(this.bDebug) document.getElementById("debug").innerHTML += e.data.message+"<br>"//+lap+"ms";
+                
             }
         }
-        */
     }
 }
