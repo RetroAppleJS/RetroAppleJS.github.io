9337083182fceefb1023518dd32aa7f6a5fab430
Hitchhiker <freddy.vandriessche@gmail.com>
2024-02-10 12:44:24 +0100

    test

--- tools/dithertron/test/unit/test-nes.ts
+++
@@ -1,4 +0,0 @@
-import { doTest } from "../test-utils";
-
-doTest('nes', 'lobsterpot.jpg', { maxiters: 10, quality: 200, diffuse: 0.5 });
-doTest('nes4f', 'lobsterpot.jpg', { maxiters: 10, quality: 200, diffuse: 0.5 });
