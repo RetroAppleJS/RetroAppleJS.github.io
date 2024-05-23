a53caf4cd9d0bbe0dd448f0278fac2a1b6a4af69
Hitchhiker <freddy.vandriessche@gmail.com>
2024-05-23 23:18:44 +0200

    Update EMU_apple2main.js

--- res/EMU_apple2main.js
+++ res/EMU_apple2main.js
@@ -94,13 +94,15 @@ function EMU_init()
                 try
                 {
                     oEMU.system[uri] = oCOM.URL.uri[uri]!="0" && oCOM.URL.uri[uri]!="false";
+
+                    // TODO: check why this fails on older browsers
                     //oEMUI.muteBtn({id:'mutebutton',class1:'fa-volume-up',class2:'fa-volume-mute',override:oEMU.system[uri]==false}).muteAct();
-                    oCOM.POPUP.html("boot+mute OK");
+                    //oCOM.POPUP.html("boot+mute OK");
                     
                 }
                 catch({ name, message })
                 {
-                    oCOM.POPUP.html("boot&mute failed: "+name+" "+message);
+                    //oCOM.POPUP.html("boot&mute failed: "+name+" "+message);
                 }
 
                 try
@@ -110,14 +112,14 @@ function EMU_init()
                     infla.push(db);            
                     var dd = infla.result;
 
-                    //oCOM.POPUP.html("boot 1.0 success: "+dd.length);
+                    oCOM.POPUP.html("boot 1.0 success: "+dd.length);
 
                     if(typeof(dd)!="undefined")
                         _o["D1_buffer"] = dd;
                 }
                 catch({ name, message })
                 {
-                    //oCOM.POPUP.html("boot 1.0 failed: "+name+" "+message);
+                    oCOM.POPUP.html("boot 1.0 failed: "+name+" "+message);
                 }
 
             break;
