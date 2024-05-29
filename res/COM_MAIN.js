39f1604967561ff2e8f80a9a5564693f6f20670d
Hitchhiker <freddy.vandriessche@gmail.com>
2024-05-29 17:40:05 +0200

    Update COM_MAIN.js

--- res/COM_MAIN.js
+++ res/COM_MAIN.js
@@ -201,6 +201,9 @@ function COM()
     }
   }
 
+  this.crc32 = function(r){for(var a,o=[],c=0;c<256;c++){a=c;for(var f=0;f<8;f++)a=1&a?3988292384^a>>>1:a>>>1;o[c]=a}for(var n=-1,t=0;t<r.length;t++)n=n>>>8^o[255&(n^r[t])];return(-1^n)>>>0};
+
+
   /////// GUI FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////
 
 
