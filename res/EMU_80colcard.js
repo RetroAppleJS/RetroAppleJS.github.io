f27a591e0ba1d28e07f5c8aaa8f95ffdfd77780d
Hitchhiker <freddy.vandriessche@gmail.com>
2024-06-13 20:11:59 +0200

    Create EMU_80colcard.js

---
+++ res/EMU_80colcard.js
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2024 Freddy Vandriessche.
+// notice: https://raw.githubusercontent.com/RetroAppleJS/RetroAppleJS.github.io/main/LICENSE.md
+//
+// EMU_80colcard.js
+
+if(oEMU===undefined) var oEMU = {"component":{"IO":{"EightyColCard":new EightyColCard()}}}
+else oEMU.component.IO.EightyColCard = new EightyColCard();
+
+function EightyColCard()
+{
+    
+}
