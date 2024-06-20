18fc1fe4a696d190fc72584f42c40448fb89fa94
Hitchhiker <freddy.vandriessche@gmail.com>
2024-06-20 15:40:26 +0200

    Update showdown.js

--- res/showdown.js
+++ res/showdown.js
@@ -4809,28 +4809,28 @@ showdown.subParser('makeMarkdown.txt', function (node) {
   // emphasis, strong and strikethrough - can appear everywhere
   // we also escape pipe (|) because of tables
   // and escape ` because of code blocks and spans
-  txt = txt.replace(/([*_~|`])/g, '\\$1');
+  txt = txt.replace(/([*_~|`])/g, "\\$1");
 
   // escape > because of blockquotes
-  txt = txt.replace(/^(\s*)>/g, '\\$1>');
+  txt = txt.replace(/^(\s*)>/g, "\\$1>");
 
   // hash character, only troublesome at the beginning of a line because of headers
-  txt = txt.replace(/^#/gm, '\\#');
+  txt = txt.replace(/^#/gm, "\\#");
 
   // horizontal rules
-  txt = txt.replace(/^(\s*)([-=]{3,})(\s*)$/, '$1\\$2$3');
+  txt = txt.replace(/^(\s*)([-=]{3,})(\s*)$/, "$1\\$2$3");
 
   // dot, because of ordered lists, only troublesome at the beginning of a line when preceded by an integer
-  txt = txt.replace(/^( {0,3}\d+)\./gm, '$1\\.');
+  txt = txt.replace(/^( {0,3}\d+)\./gm, "$1\\.");
 
   // +, * and -, at the beginning of a line becomes a list, so we need to escape them also (asterisk was already escaped)
-  txt = txt.replace(/^( {0,3})([+-])/gm, '$1\\$2');
+  txt = txt.replace(/^( {0,3})([+-])/gm, "$1\\$2");
 
   // images and links, ] followed by ( is problematic, so we escape it
-  txt = txt.replace(/]([\s]*)\(/g, '\\]$1\\(');
+  txt = txt.replace(/]([\s]*)\(/g, "\\]$1\\(");
 
   // reference URIs must also be escaped
-  txt = txt.replace(/^ {0,3}\[([\S \t]*?)]:/gm, '\\[$1]:');
+  txt = txt.replace(/^ {0,3}\[([\S \t]*?)]:/gm, "\\[$1]:");
 
   return txt;
 });
