6071dfb4538dba39d91d0f257cd900ae3808722f
Hitchhiker <freddy.vandriessche@gmail.com>
2024-07-03 09:23:54 +0200

    Update EMU_apple2io.js

--- res/EMU_apple2io.js
+++ res/EMU_apple2io.js
@@ -90,63 +90,86 @@ function Apple2IO(vid)
         this.disk2.reset();
     }
     
+    var test = new Uint8Array([0,0,0,"A".charCodeAt(0),"B".charCodeAt(0),"C".charCodeAt(0)]);
+    var check = new Uint8Array(["A".charCodeAt(0),"B".charCodeAt(0),"C".charCodeAt(0)])
+    document.getElementById("footer").innerHTML = (indexedDB.cmp(test.slice(3,6),check) === 0)
+
     this.read = function(addr)
     {
-        if (addr >= KEY_DATA && addr < KEY_DATA + 0x10)
-            return key;
-        else if (addr >= KEY_STROBE && addr < KEY_STROBE + 0x10)
-            key &= 0x7f;
-        else if (addr >= DISK_IO && addr < DISK_IO + DISK_IO_SIZE)
+        // TODO: TRANSLATE ADDR RANGE DIRECTLY TO ENUM VALUE >>> USE SWITCH-CASE STATEMENT
+        var line = addr-SLOT_IO[0] >> 4 ; // line decoder
+
+        switch(line)
         {
-            var o = this.disk2.read(addr - DISK_IO);
-            return o;
+            case 6:
+                var o = this.disk2.read(addr - DISK_IO);
+                return o;
+
+            default:
+
+                if (addr >= KEY_DATA && addr < KEY_DATA + 0x10)
+                    return key;
+                else if (addr >= KEY_STROBE && addr < KEY_STROBE + 0x10)
+                    key &= 0x7f;
+                else if (addr >= DISK_IO && addr < DISK_IO + DISK_IO_SIZE)
+                {
+                    //var o = this.disk2.read(addr - DISK_IO);
+                    //return o;
+                }
+                else if (this.disk2.diskBytes[this.disk2.drv] && addr >= DISK_PROM &&
+                        addr < DISK_PROM + DISK_PROM_SIZE)
+                    return this.disk2.ROM[addr - DISK_PROM];
+                else if(this.ramcard.active  // RAMCARD SOFT SWITCHES
+                    && addr >= MEM_RAMCARD_IO && addr < MEM_RAMCARD_IO + MEM_RAMCARD_IO_SIZE)
+                    return this.ramcard.soft_switch(addr - MEM_RAMCARD_IO);
+                else if(this.ramcard.active &&
+                    addr >= ROM_ADDR && addr < ROM_ADDR + ROM_SIZE)
+                    return this.ramcard.read(addr - ROM_ADDR);
+                else if(this.col80card.active &&
+                    addr >= MEM_COL80CARD_IO && addr < MEM_COL80CARD_IO + ROM_SIZE)
+                    {
+                        // TODO: read ROM from 80-column card !!
+                        //alert("80_COL $"+oCOM.getHexWord(addr));
+                    }
+                else
+                    switch(addr)
+                    {
+                    case SPKR_TOGGLE:
+                        snd.toggle();
+                        break;
+                    case GFX_ON:
+                        video.setGfx(true);
+                        break;
+                    case GFX_OFF:
+                        video.setGfx(false);
+                        break;
+                    case GFX_MIX_OFF:
+                        video.setMix(false);
+                        break;
+                    case GFX_MIX_ON:
+                        video.setMix(true);
+                        break;
+                    case GFX_PAGE1:
+                        video.setPage2(false);
+                        break;
+                    case GFX_PAGE2:
+                        video.setPage2(true);
+                        break;
+                    case GFX_LORES:
+                        video.setHires(false);
+                        break;
+                    case GFX_HIRES:
+                        video.setHires(true);
+                        break;
+                    }
+
+
+
+
+            break;
         }
-        else if (this.disk2.diskBytes[this.disk2.drv] && addr >= DISK_PROM &&
-                 addr < DISK_PROM + DISK_PROM_SIZE)
-            return this.disk2.ROM[addr - DISK_PROM];
-        else if(this.ramcard.active  // RAMCARD SOFT SWITCHES
-            && addr >= MEM_RAMCARD_IO && addr < MEM_RAMCARD_IO + MEM_RAMCARD_IO_SIZE)
-            return this.ramcard.soft_switch(addr - MEM_RAMCARD_IO);
-        else if(this.ramcard.active &&
-            addr >= ROM_ADDR && addr < ROM_ADDR + ROM_SIZE)
-            return this.ramcard.read(addr - ROM_ADDR);
-        else if(this.col80card.active &&
-            addr >= MEM_COL80CARD_IO && addr < MEM_COL80CARD_IO + ROM_SIZE)
-            {
-                // TODO: read ROM from 80-column card !!
-                //alert("80_COL $"+oCOM.getHexWord(addr));
-            }
-        else
-            switch(addr)
-            {
-            case SPKR_TOGGLE:
-                snd.toggle();
-                break;
-            case GFX_ON:
-                video.setGfx(true);
-                break;
-            case GFX_OFF:
-                video.setGfx(false);
-                break;
-            case GFX_MIX_OFF:
-                video.setMix(false);
-                break;
-            case GFX_MIX_ON:
-                video.setMix(true);
-                break;
-            case GFX_PAGE1:
-                video.setPage2(false);
-                break;
-            case GFX_PAGE2:
-                video.setPage2(true);
-                break;
-            case GFX_LORES:
-                video.setHires(false);
-                break;
-            case GFX_HIRES:
-                video.setHires(true);
-                break;
-            }
+
+     
 
         return 0x00;
     }
@@ -177,22 +200,25 @@ function Apple2IO(vid)
         alert("AppleDisk2() does not have a method called loadDisk")
     }
 
-    this.register_slot = function(slot_num,device_obj)
+    this.mount = function(device_obj,slot_num,active)
     {   
-        const idx = slot_num<<2; 
-        SLOT_MAP[idx]   = SLOT_IO[slot_num];                                                // SLOT I/O range origin
-        SLOT_MAP[idx+1] = SLOT_IO[slot_num] + SLT_IO_SIZE;                                  //          range end
-        SLOT_MAP[idx+2] = device_obj.ROM===undefined?0:SLOT_PROM[slot_num];                 // SLOT ROM range origin
-        SLOT_MAP[idx+3] = device_obj.ROM===undefined?0:SLOT_PROM[slot_num] + SLT_PROM_SIZE; //          range end
+        const idx = slot_num<<3; 
+        const noROM = device_obj.ROM===undefined;
+        SLOT_MAP[idx]   = active ? 0x2 : 0x1;                          // STATUS 0x2=active  0x1=inactive 0x0=unmounted   
+        SLOT_MAP[idx+1] = SLOT_IO[slot_num];                           // SLOT I/O range origin
+        SLOT_MAP[idx+2] = SLOT_IO[slot_num] + SLT_IO_SIZE;             //          range end
+        SLOT_MAP[idx+3] = noROM?0:SLOT_PROM[slot_num];                 // SLOT ROM range origin
+        SLOT_MAP[idx+4] = noROM?0:SLOT_PROM[slot_num] + SLT_PROM_SIZE; //          range end
     }
 
-    this.unregister_slot = function(slot_num,device_obj)
+    this.unmount = function(device_obj,slot_num)
     {
-        const idx = slot_num<<2; 
+        const idx = slot_num<<2;
         SLOT_MAP[idx]   = 0;
         SLOT_MAP[idx+1] = 0;
         SLOT_MAP[idx+2] = 0;
-        SLOT_MAP[idx+3] = 0; 
+        SLOT_MAP[idx+3] = 0;
+        SLOT_MAP[idx+4] = 0;
     }
 
     // SLOT MAPPING
@@ -201,5 +227,5 @@ function Apple2IO(vid)
 
 
     // SLOT MAPPING
-    this.register_slot(6,this.disk2);
+    this.mount(this.disk2,6);
 }
