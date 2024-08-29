d475d0d76077ac78dac6aabbee08e47ee44b55c4
Hitchhiker <freddy.vandriessche@gmail.com>
2024-08-29 14:16:48 +0200

    optimise basic number conversion functions

--- res/COM_MAIN.js
+++ res/COM_MAIN.js
@@ -1,41 +1,38 @@
 function COM()
 {
-  this.hextab= ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-  this.getHexByte    = function(v) { return this.hextab[v>>4]+this.hextab[v&0xf] }
-  this.HEX2RGB       = function(hex) { var n=parseInt(hex.slice(1),16); return [(n>>16)&0xFF,(n>>8)&0xFF,n&0xFF] }
-  this.RGB2HEX       = function(color) { return [this.getHexByte(color[0]),this.getHexByte(color[1]),this.getHexByte(color[2])] }
+  const hexLookupTable = new Array(256);
+  for (let i = 0; i < 256; i++) {
+    hexLookupTable[i] = i.toString(16).padStart(2, '0');
+  }
+
+  const hex2tab=[
+    "00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F","10","11","12","13","14","15","16","17","18","19","1A","1B","1C","1D","1E","1F",
+    "20","21","22","23","24","25","26","27","28","29","2A","2B","2C","2D","2E","2F","30","31","32","33","34","35","36","37","38","39","3A","3B","3C","3D","3E","3F",
+    "40","41","42","43","44","45","46","47","48","49","4A","4B","4C","4D","4E","4F","50","51","52","53","54","55","56","57","58","59","5A","5B","5C","5D","5E","5F",
+    "60","61","62","63","64","65","66","67","68","69","6A","6B","6C","6D","6E","6F","70","71","72","73","74","75","76","77","78","79","7A","7B","7C","7D","7E","7F",
+    "80","81","82","83","84","85","86","87","88","89","8A","8B","8C","8D","8E","8F","90","91","92","93","94","95","96","97","98","99","9A","9B","9C","9D","9E","9F",
+    "A0","A1","A2","A3","A4","A5","A6","A7","A8","A9","AA","AB","AC","AD","AE","AF","B0","B1","B2","B3","B4","B5","B6","B7","B8","B9","BA","BB","BC","BD","BE","BF",
+    "C0","C1","C2","C3","C4","C5","C6","C7","C8","C9","CA","CB","CC","CD","CE","CF","D0","D1","D2","D3","D4","D5","D6","D7","D8","D9","DA","DB","DC","DD","DE","DF",
+    "E0","E1","E2","E3","E4","E5","E6","E7","E8","E9","EA","EB","EC","ED","EE","EF","F0","F1","F2","F3","F4","F5","F6","F7","F8","F9","FA","FB","FC","FD","FE","FF"];
+  
+  this.hextab= ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']; // TODO: remove from entire codebase
+  this.getHexByte   = function(v)     { return hex2tab[v&0xFF] }
+  this.getHexWord   = function(v)     { return hex2tab[v>>8] + hex2tab[v&0xFF] }
+  this.getHexMulti = function(v,m)    { return ("0".repeat(m)+v.toString(16)).slice(-m).toUpperCase() }
+  this.getBinMulti = function(v,m)    { return ("0".repeat(m)+v.toString(2)).slice(-m).toUpperCase() }
+  this.HEX2RGB       = function(hex)   { var n=parseInt(hex.slice(1),16); return [(n>>16)&0xFF,(n>>8)&0xFF,n&0xFF] }
+  this.RGB2HEX       = function(color) { return [this.hex2tab[color[0]&0xFF],this.hex2tab[color[1]&0xFF],this.hex2tab[color[2]&0xFF]] }
+
   this.RGB2IDX       = function(color,sig_bits)
   {
       const msk = (1<<sig_bits)-1, scl = (8-sig_bits);
       return (((color[2]>>scl)&msk)<<sig_bits<<sig_bits) | (((color[1]>>scl)&msk)<<sig_bits) | ((color[0]>>scl)&msk)
-  }  
+  }
+
   this.IDX2RGB       = function(idx,sig_bits)
   {
       const msk = (1<<sig_bits)-1, scl = (8-sig_bits);
       return [(idx & msk)<<scl,((idx>>sig_bits) & msk)<<scl,((idx>>sig_bits>>sig_bits) & msk)<<scl];
-  }
-
-  this.getHexWord = function(v)
-  {
-    return '' + this.hextab[v >> 12]
-              + this.hextab[(v & 0x0f00)>>8]
-              + this.hextab[(v & 0xf0)>>4]
-              + this.hextab[v & 0x000f];
-  }
-
-  this.getHexMulti = function(v,m)
-  {
-    return ("0".repeat(m)+v.toString(16)).slice(-m).toUpperCase();
-  }
-
-  this.getBinMulti = function(v,m)
-  {
-    var s = "";
-    var r = ("0".repeat(m)+v.toString(2)).slice(-m).toUpperCase();
-    //for(var i=0;i<r.length;i++)
-    //  s+= ("0000"+parseInt(r.charAt(i),16).toString(2)).slice(-4)
-    //return s;
-    return r;
   }
 
   this.base64ToArrayBuffer = function(base64)
@@ -60,7 +57,6 @@ function COM()
           binary += String.fromCharCode( bytes[ i ] );
       }
       return window.btoa( binary );
-
   }
 
   this.b64EncodeUnicode = function(str)
