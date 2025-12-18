eb70f81144e987e87bd84c8cdb9adb1a507433fa
Zen <freddy.vandriessche@gmail.com>
2025-12-18 15:34:41 +0100

    Create ASCII_schema_catalog.js

---
+++ tools/GUI_DEV/ASCII_schema_catalog.js
@@ -0,0 +1,24 @@
+// Hardcoded catalog (extend freely)
+  const CATALOG = [
+    {
+      name: 'ATTiny85',
+      type: 'MCU',
+      description: '',
+      MFR: 'ATTINY85V-10PU',
+      pin_data: null,
+      text_data:
+        '┌───┴─────┴─────┴─────┴───┐\n' +
+        '│        @4    @3         │\n' +
+        '│╔═ATTiny85══════════════╗│\n' +
+        '│║ GND   PB4   PB3   PB5 ║│\n' +
+        '│║             TX     ⬤ ║│\n' +
+        '│║ SDA                   ║│\n' +
+        '│║ MISO  MOSI  SCL       ║│\n' +
+        '│║ PB0   PB1   PB2   VCC ║│\n' +
+        '│╚═══════════════════════╝│\n' +
+        '│  @0    @1    @2         │\n' +
+        '└───┬─────┬─────┬─────┬───┘\n',
+      image_data:
+      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABAAgMAAADXB5lNAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAxQTFRFAAAA9fX1ZmVmPj0+fAufOQAAAAR0Uk5TAP/7Xe+ZR0gAAAGFSURBVHicldI9SgNBFADgga1sklOk8AAWm0JP8R7upE4gq+AVvIHVDGFjO8Gsi9gaLALpLJJTRDBVwCrBwvdmdnd21kJ9kMB88/4WRogyuksRRv85PEfmKoRzwDDlA+AmSFAAct+AA1AU/txVDAM/OQYbddvIOKgnd9wZsKrZllBN7qgKyskHqOPVz3RNisZMW3LdmMlhLoOWIGHA4DPmFk7GkDtDZVeLMYPEwpiKGGCUz1xLgBHBGeSQupaAM4IepcoyIZUWJGCZMGfY0V3iWqYJw9aATO1MyC30UkCj7Eyd8GI9uhuObQIm/C08hdrxrwJJt0OeiXOGU7uBoaUkHsrPlwypzvDIEG35qKhlhuuJfSpUDkpKzHFlIaZ6pXKJiwpeaO9MS3wbRA7uqG1OsK7glp6PJtjcV7BTBNSyhndDsGjCkYBaetgYrddTD7tlnOnVxMOF6D4FQH+fOgohmnro/4BlC9xTdVC0oPH6/whFC8RXG8R/4aEFYt8G8Qs8tqBe6huGHP6l63xHWgAAAABJRU5ErkJggg=='
+    }
+  ];
