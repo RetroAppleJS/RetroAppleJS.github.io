f177c81bff32a580b24e706b877d8ec27d414074
Zen <freddy.vandriessche@gmail.com>
2026-04-20 17:51:15 +0200

    layout fix (not good)

--- res/EMU_apple2main.js
+++ res/EMU_apple2main.js
@@ -551,9 +551,7 @@ function EMUI()
         {
             box.innerHTML = this.deviceToolHTML();
             box.setAttribute("data-topology",sig);
-            if(typeof(EMU_mem_map)=="function"
-            && typeof(apple2plus)=="object"
-            && apple2plus!=null)
+            if(typeof(EMU_mem_map)=="function" && typeof(apple2plus)=="object" && apple2plus!=null)
                 EMU_mem_map();
         }
 
@@ -631,9 +629,9 @@ function EMUI()
             case "MS16K":
                 return ""
                 + "<div class=toolbox id=\"device_tool_"+slot+"\" hidden>"
-                + "  <div class=appbox style=\"width:372px;height:132px;padding:0px 6px 0px 6px;\" title=\"Memory map\">"
+                + "  <div class=appbox style=\"width:430px;height:76px;padding:0px 6px 0px 6px;\" title=\"Memory map\">"
                 + "    <div style=\"float:left;width:28px;text-align:center\">MEM<br><i class=\"fa fa-sync-alt\" id=\"MEM_monitoring\" onclick=\"oCOM.POPUP.toggle_class(this,'fa-stop-circle','fa-sync-alt');apple2plus.enable_MEM_monitoring(oCOM.toggleRefreshEvent('MEM_monitoring'));\"></i></div>"
-                + "    <div id=\"EMU_mem_map\" style=\"margin-left:30px\"></div>"
+                + "    <div id=\"EMU_mem_map\" style=\"margin-left:30px;white-space:nowrap\"></div>"
                 + "  </div>"
                 + "</div>";
 
