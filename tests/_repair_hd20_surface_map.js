50ec7ccdf78f431fe7f18834de9479cf0a46241d
Zen <freddy.vandriessche@gmail.com>
2026-09-23 14:24:19 +0200

    chore: remove temporary HD20 syntax repair helper

--- tests/_repair_hd20_surface_map.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const fs=require('node:fs');
-const path='res/EMU_CARD_LIRON.js';
-let src=fs.readFileSync(path,'utf8');
-const matches=src.match(/!==="/g)||[];
-if(matches.length<1) throw new Error('no malformed strict-inequality tokens found');
-src=src.replace(/!==="/g,'!=="');
-fs.writeFileSync(path,src);
-console.log('repaired',matches.length,'HD20 surface-map strict-inequality tokens');
