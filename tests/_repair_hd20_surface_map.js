2662a8fdb7b3116746c0d62aa521dd42c8067e36
Zen <freddy.vandriessche@gmail.com>
2026-09-23 14:22:10 +0200

    chore: add temporary HD20 surface-map syntax repair

---
+++ tests/_repair_hd20_surface_map.js
@@ -0,0 +1,10 @@
+'use strict';
+const fs=require('node:fs');
+const path='res/EMU_CARD_LIRON.js';
+let src=fs.readFileSync(path,'utf8');
+const bad='typeof(device.getSurfaceMapGeometry)!==="function"';
+const good='typeof(device.getSurfaceMapGeometry)!="function"';
+if(!src.includes(bad)) throw new Error('repair target not found');
+src=src.replace(bad,good);
+fs.writeFileSync(path,src);
+console.log('repaired HD20 surface-map syntax');
